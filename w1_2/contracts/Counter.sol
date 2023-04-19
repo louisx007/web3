e4a7c6154d70de05b72fb3d31482cc3f8e956a5f
louis <louisx0504@gmail.com>
2023-04-19 23:03:03 +0800

    作业

--- w1_2/contracts/Counter.sol
+++ w1_2/contracts/Counter.sol
@@ -1,7 +1,14 @@
 pragma solidity ^0.8.7;
 
 contract Counter {
+
     uint256 count;
+
+
+    constructor(uint256 _count){
+        count = _count;
+    }
+
     function getCount() public view returns(uint256)  {
         return count;
     }
